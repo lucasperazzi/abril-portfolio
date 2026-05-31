4f6de972dac621fcc60ccafff9041c861c83cc52
lucasperazzi <lucasperazzi98@gmail.com>
2026-05-31 15:12:13 -0300

    Every page brings to the top when opening them

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,7 @@ function setMetaTag(selector, attribute, value) {
 function ScrollToTop() {
   const { pathname } = useLocation()
   useEffect(() => {
-    window.scrollTo(0, 0)
+    window.scrollTo({ top: 0, behavior: 'smooth' })
   }, [pathname])
   return null
 }
